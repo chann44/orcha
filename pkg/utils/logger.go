ff3217662d7d7ad9d8fe1a33216a53bbed4a2bf6
chann44 <44chansw@gmail.com>
2026-02-03 02:58:38 +0530

    fixes

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -1,1 +1,62 @@
 package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Logger wraps logrus.Logger with Grafana/Loki compatible formatting
+type Logger struct {
+	*logrus.Logger
+}
+
+// NewLogger creates a new logger configured for Grafana/Loki
+func NewLogger(level string) *Logger {
+	log := logrus.New()
+
+	// Set log level
+	switch level {
+	case "debug":
+		log.SetLevel(logrus.DebugLevel)
+	case "info":
+		log.SetLevel(logrus.InfoLevel)
+	case "warn":
+		log.SetLevel(logrus.WarnLevel)
+	case "error":
+		log.SetLevel(logrus.ErrorLevel)
+	default:
+		log.SetLevel(logrus.InfoLevel)
+	}
+
+	// JSON formatter for Grafana/Loki compatibility
+	log.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: time.RFC3339Nano,
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "timestamp",
+			logrus.FieldKeyLevel: "level",
+			logrus.FieldKeyMsg:   "message",
+		},
+	})
+
+	// Output to stdout (for containerized environments)
+	log.SetOutput(os.Stdout)
+
+	return &Logger{Logger: log}
+}
+
+// WithFields adds structured fields to the logger
+func (l *Logger) WithFields(fields map[string]interface{}) *logrus.Entry {
+	return l.Logger.WithFields(logrus.Fields(fields))
+}
+
+// WithError adds an error field to the logger
+func (l *Logger) WithError(err error) *logrus.Entry {
+	return l.Logger.WithError(err)
+}
+
+// WithContext adds context fields (request_id, etc.)
+func (l *Logger) WithContext(fields map[string]interface{}) *logrus.Entry {
+	return l.Logger.WithFields(logrus.Fields(fields))
+}
